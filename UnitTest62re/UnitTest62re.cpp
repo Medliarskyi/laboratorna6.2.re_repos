1c9e626664bf574bf14d793745e016d06647a2b8
Medliarskyi <med.uras111@gmail.com>
2024-10-16 00:23:29 +0300

    first master commit

---
+++ UnitTest62re/UnitTest62re.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab62re/lab62re.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62re
+{
+	TEST_CLASS(UnitTest62re)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int arr[] = { 1, 2, 3, 4, 5 };
+			int expected[] = { 5, 4, 3, 2, 1 };
+
+			reverseArrayRec(arr, 0, 4);  
+
+			for (int i = 0; i < 5; ++i) {
+				Assert::AreEqual(expected[i], arr[i]);
+			}
+		}
+	};
+}
---
+++ lab62re/lab62re.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib> // для rand
+
+using namespace std;
+
+void createArrayRec(int* arr, int n) {
+    if (n <= 0) return;
+    createArrayRec(arr, n - 1);
+    arr[n - 1] = rand() % 100;
+}
+
+void printArrayRec(const int* arr, int n, int index = 0) {
+    if (index == n) {
+        cout << endl;
+        return;
+    }
+    cout << setw(4) << arr[index];
+    printArrayRec(arr, n, index + 1);
+}
+
+void reverseArrayRec(int* arr, int start, int end) {
+    if (start >= end) return;
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    reverseArrayRec(arr, start + 1, end - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int* arr = new int[n];
+    createArrayRec(arr, n);
+    cout << "Initial array: ";
+    printArrayRec(arr, n);
+
+    reverseArrayRec(arr, 0, n - 1);
+    cout << "The array is in reverse order: ";
+    printArrayRec(arr, n);
+
+    delete[] arr;
+    return 0;
+}
